6516d1183ff2b6d9850cd98280e178e4b94a9c87
tsar-boomba <itg.2048@gmail.com>
2025-03-17 18:22:01 -0700

    Add styles file for playback

---
+++ web/src/components/Playback/Playback.css.ts
@@ -0,0 +1,14 @@
+import { style } from "@vanilla-extract/css";
+import { vars } from "../../theme";
+
+export const base = style({
+	backgroundColor:
+		'light-dark(var(--mantine-color-body), var(--mantine-color-body))',
+	boxShadow: vars.shadows.sm,
+	borderTop: `1px solid ${vars.colors.dark[4]}`,
+	[vars.lightSelector]: {
+		borderTop: `1px solid ${vars.colors.gray[3]}`,
+	},
+	paddingBottom: vars.spacing.xs,
+	paddingTop: vars.spacing.xs,
+});
